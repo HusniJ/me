4a639947a388d71b997acc8a39ff4c5bc0f59bd5
husni <husnijabir@gmail.com>
2022-04-10 15:10:48 +0530

    Updates

---
+++ precache-manifest.cc77657019ae2c9ca4459391a00df451.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "93155faad49a73d4b16e",
+    "url": "/me/static/css/main.d2fe8770.chunk.css"
+  },
+  {
+    "revision": "93155faad49a73d4b16e",
+    "url": "/me/static/js/main.93155faa.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "834c21d43cee1bd8103c56e71a229d1c",
+    "url": "/me/index.html"
+  }
+];
