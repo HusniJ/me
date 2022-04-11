22924c4e40337f77759887ff84f9c517c6304d06
husni <husnijabir@gmail.com>
2022-04-11 12:50:33 +0530

    Updated project section

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,24 +1,24 @@
 import React, { Component } from 'react'
 
 export default class Projects extends Component {
-  render() {
-    return (
-      <div>
+	render() {
+		return (
+			<div>
 				<section className="colorlib-work" data-section="projects">
 					<div className="colorlib-narrow-content">
 						<div className="row">
 							<div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-								<span className="heading-meta">My Work</span>
+								<span className="heading-meta">Kids Portal</span>
 								<h2 className="colorlib-heading animate-box">Recent Projects</h2>
 							</div>
 						</div>
 						<div className="row">
 							<div className="col-md-4 animate-box" data-animate-effect="fadeInLeft">
-								<div className="project" style={{backgroundImage: 'url(images/img-1.jpg)'}}>
+								<div className="project" style={{ backgroundImage: 'url(images/img-1.jpg)' }}>
 									<div className="desc">
 										<div className="con">
-											<h3><a href="work.html">Work 01</a></h3>
-											<span>Website</span>
+											<h3><a href="https://www.cordonbleukurier.ch/" target="_blank">View Portal</a></h3>
+											<span>Cordon Bleu Kurier</span>
 											<p className="icon">
 												<span><a href="#"><i className="icon-share3" /></a></span>
 												<span><a href="#"><i className="icon-eye" /> 100</a></span>
@@ -29,11 +29,11 @@ export default class Projects extends Component {
 								</div>
 							</div>
 							<div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
-								<div className="project" style={{backgroundImage: 'url(images/img-2.jpg)'}}>
+								<div className="project" style={{ backgroundImage: 'url(images/img-2.jpg)' }}>
 									<div className="desc">
 										<div className="con">
-											<h3><a href="work.html">Work 02</a></h3>
-											<span>Animation</span>
+											<h3><a href="https://husnij.github.io/kidsportal/" target="_blank">View Web</a></h3>
+											<span>Kids Portal</span>
 											<p className="icon">
 												<span><a href="#"><i className="icon-share3" /></a></span>
 												<span><a href="#"><i className="icon-eye" /> 100</a></span>
@@ -44,11 +44,11 @@ export default class Projects extends Component {
 								</div>
 							</div>
 							<div className="col-md-4 animate-box" data-animate-effect="fadeInTop">
-								<div className="project" style={{backgroundImage: 'url(images/img-3.jpg)'}}>
+								<div className="project" style={{ backgroundImage: 'url(images/img-3.jpg)' }}>
 									<div className="desc">
 										<div className="con">
-											<h3><a href="work.html">Work 03</a></h3>
-											<span>Illustration</span>
+											<h3><a href="https://json-server-node.herokuapp.com" target={"_blank"}>Json Server</a></h3>
+											<span>Mock API Host</span>
 											<p className="icon">
 												<span><a href="#"><i className="icon-share3" /></a></span>
 												<span><a href="#"><i className="icon-eye" /> 100</a></span>
@@ -59,11 +59,11 @@ export default class Projects extends Component {
 								</div>
 							</div>
 							<div className="col-md-4 animate-box" data-animate-effect="fadeInBottom">
-								<div className="project" style={{backgroundImage: 'url(images/img-4.jpg)'}}>
+								<div className="project" style={{ backgroundImage: 'url(images/img-4.jpg)' }}>
 									<div className="desc">
 										<div className="con">
-											<h3><a href="work.html">Work 04</a></h3>
-											<span>Application</span>
+											<h3><a href="https://husnij.github.io/images-app/" target="_blank">Images App</a></h3>
+											<span>View App</span>
 											<p className="icon">
 												<span><a href="#"><i className="icon-share3" /></a></span>
 												<span><a href="#"><i className="icon-eye" /> 100</a></span>
@@ -74,7 +74,7 @@ export default class Projects extends Component {
 								</div>
 							</div>
 							<div className="col-md-4 animate-box" data-animate-effect="fadeInLeft">
-								<div className="project" style={{backgroundImage: 'url(images/img-5.jpg)'}}>
+								<div className="project" style={{ backgroundImage: 'url(images/img-5.jpg)' }}>
 									<div className="desc">
 										<div className="con">
 											<h3><a href="work.html">Work 05</a></h3>
@@ -89,7 +89,7 @@ export default class Projects extends Component {
 								</div>
 							</div>
 							<div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
-								<div className="project" style={{backgroundImage: 'url(images/img-6.jpg)'}}>
+								<div className="project" style={{ backgroundImage: 'url(images/img-6.jpg)' }}>
 									<div className="desc">
 										<div className="con">
 											<h3><a href="work.html">Work 06</a></h3>
@@ -106,12 +106,12 @@ export default class Projects extends Component {
 						</div>
 						<div className="row">
 							<div className="col-md-12 animate-box">
-								<p><a href="#" className="btn btn-primary btn-lg btn-load-more">Load more <i className="icon-reload" /></a></p>
+								<p><a href="https://github.com/HusniJ" target="_blank" className="btn btn-primary btn-lg btn-load-more">Load more <i className="icon-reload" /></a></p>
 							</div>
 						</div>
 					</div>
 				</section>
-      </div>
-    )
-  }
+			</div>
+		)
+	}
 }
